fb2a215eff0246d52d271a3654a769d13c1d1fb8
Artem Zaytsev <arepo@nologin.ru>
2015-01-02 15:40:54 +0400

    Добавил запуск тестов аггрегационных функций и код возврата

--- test/all.sh
+++ test/all.sh
@@ -2,9 +2,26 @@
 
 wd=$(dirname "$0")
 
-(
-	echo "SELECT '--- Basic functions ---';";
-	cat "$wd/sql/basicFunction.sql";
-	echo "SELECT '';";
+catSql() {
+	echo "SELECT '--- Basic functions ---';"
+	cat "$wd/sql/basicFunctions.sql"
+	echo "SELECT '';"
 	echo "SELECT '--- Aggregation functions ---';"
-) | mysql -N $@
+	cat "$wd/sql/aggregationFunctions.sql"
+}
+
+result=`catSql | mysql -N $@` || exit 1
+
+echo "$result"
+echo
+echo '--- Result ---'
+
+success=`echo "$result" | grep -c OK`
+failed=`echo "$result" | grep -c FAIL`
+
+if [ "$failed" -gt 0 ]; then
+	echo Failed tests: $failed / $(($success + $failed))
+	exit 2
+else
+	echo Everything is OK "($success tests)"
+fi
